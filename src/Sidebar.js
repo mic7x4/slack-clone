37538408390d8ba067260602ff143fcc6748078b
unknown <mic7x4@gmail.com>
2020-11-26 00:58:52 +0200

    adding sidebar option and pass props to the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,16 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import SidebarOption from './SidebarOption';
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+
 
 function Sidebar() {
     return (
@@ -20,7 +30,19 @@ function Sidebar() {
                 <CreateIcon/>
             </div>
                 <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
-                <SidebarOption title="Youtube"/>
+                <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+                <SidebarOption Icon={DraftsIcon} title="Saved Items"/>
+                <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>
+                <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+                <SidebarOption Icon={AppsIcon} title="Apps"/>
+                <SidebarOption Icon={FileCopyIcon} title="FileBrowser"/>
+                <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+                <hr/>
+                <SidebarOption Icon={ExpandMoreIcon} title="Channels"  />
+                <hr/>
+                <SidebarOption Icon = {AddIcon} addChannelOption title = 'Add Channel' />
+                {/* {Connect to db and list all the channels} */}
+                {/* {Sidebar Option} */}
         </div>
     )
 }
