62ef1869b8f6f3ab4408ad81eaf2d95794f7ee0e
unknown <mic7x4@gmail.com>
2020-11-25 23:40:10 +0200

    working on sidebar header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     //BEM naming
     <div className="app">
-    {/* {Header} */}
     <Header/>
-    {/* {Sidebar} */}
-    {/* {React Router -> Chat Screen} */}
+    <div className="app__body">   
+      <Sidebar/>
+      {/* {Sidebar} */}
+      {/* {React Router -> Chat Screen} */}
+    </div>
     </div>
   );
 }
