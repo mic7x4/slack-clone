1625821c6a8829878201b353847f40d01cb89fb8
unknown <mic7x4@gmail.com>
2020-11-26 17:12:01 +0200

    working on the message side

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import './Chat.css';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import db from './firebase';
+import './Chat.css';
+import Message from './Message';
 
 function Chat() {
     const {roomId} = useParams();
@@ -43,6 +44,17 @@ console.log("MESSAGES >>>", roomMessages)
                         <InfoOutlinedIcon/> Details
                     </p>
                 </div>
+            </div>
+            <div className="chat_messages">
+               {roomMessages.map(
+                   ({message,timestamp,user,userImage}) => (
+                   <Message 
+                   message= {message}
+                   timestamp ={timestamp}
+                   user={user}
+                   userImage={userImage} 
+                   />
+               ))}
             </div>
         </div>
     )
